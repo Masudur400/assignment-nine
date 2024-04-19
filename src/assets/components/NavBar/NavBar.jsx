2359c88903a7eec86e025f4c4cf0cb270449478b
Masudur400 <md8004210@gmail.com>
2024-04-20 01:05:41 +0600

    last update

--- src/assets/components/NavBar/NavBar.jsx
+++ src/assets/components/NavBar/NavBar.jsx
@@ -12,17 +12,17 @@ const NavBar = () => {
 
     return (
         <div>
-            <div className="navbar bg-red-400 text-white font-bold rounded-md">
-                <div className="navbar-start">
+            <div className="navbar flex max-sm:flex-col bg-red-400 text-white font-bold rounded-md">
+                <div className="navbar-start max-sm:w-full">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5  " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-red-500  rounded-box w-52">
                             {links}
                         </ul>
                     </div>
-                    <Link className="btn btn-ghost text-xl"> Residential</Link>
+                    <p className="text-center max-sm:w-[70%]"><Link to="/" className="btn btn-ghost text-xl "> Residential</Link></p>
 
                 </div>
                 <div className="navbar-center hidden lg:flex">
@@ -30,7 +30,7 @@ const NavBar = () => {
                         {links}
                     </ul>
                 </div>
-                <div className=" navbar-end flex gap-3">
+                <div className=" navbar-end flex gap-3 max-sm:justify-between max-sm:w-full">
                      
                         <img className="w-12 h-12 rounded-full"  alt="" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                      
